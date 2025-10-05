5b855bfde79cfa0b8880b9bd523607aff8625a3f
Neelayjain02 <141833494+Neelayjain02@users.noreply.github.com>
2025-10-06 03:53:55 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -26,7 +26,10 @@ def predict():
         # Catch all errors and return message
         return jsonify({"error": str(e)}), 500
         
-port = int(os.environ.get("PORT", 5000))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default 5000
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
