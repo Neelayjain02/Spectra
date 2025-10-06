4d192bb493e47effe133b7ca6d514b5c366f6652
Neelayjain02 <141833494+Neelayjain02@users.noreply.github.com>
2025-10-06 23:04:54 +0530

    Update FeatureInputPage.jsx

--- frontend/src/FeatureInputPage.jsx
+++ frontend/src/FeatureInputPage.jsx
@@ -217,7 +217,7 @@ const FeatureInputPage = () => {
     const inputData = { ...starValues, ...planetValues, ...orbitValues };
 
     try {
-      const res = await fetch("https://neelay21-spectra.hf.space//predict_fn", {
+      const res = await fetch("https://neelay21-spectra.hf.space//predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(inputData),
@@ -338,5 +338,6 @@ const FeatureInputPage = () => {
 };
 
 export default FeatureInputPage;
+
 
 
